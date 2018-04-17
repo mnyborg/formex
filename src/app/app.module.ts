30c06c24c6bb4dd9907c64d3401aaff97d0c43e3
mnyborg <manyb@dtu.dk>
2018-04-17 11:12:33 +0200

    v 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ModelFormComponent } from './modelform/modelform.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
